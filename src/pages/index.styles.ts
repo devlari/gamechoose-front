bbf1048fcdba473d5bcd0574f6ea5e9e24d26249
Larissa <larisousa2401@hotmail.com>
2023-02-13 21:53:31 -0300

    Textos da página inicial

--- src/pages/index.styles.ts
+++ src/pages/index.styles.ts
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
-export const Title = styled.h1`
+export const TitleImage = styled.h1`
   font-size: 4rem;
   text-align: center;
   margin-bottom: -20px;
 `;
 
-export const Subtitle = styled.h3`
+export const SubtitleImage = styled.h3`
   font-size: 1.5rem;
   text-align: center;
 `;
@@ -23,4 +23,29 @@ export const BackgroundImage = styled.div`
   background-position: center;
   background-attachment: fixed;
   position: relative;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Column = styled.div`
+  width: 80%;
+  padding: 20px;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 3rem;
+`;
+
+export const Content = styled.p`
+  font-size: 1.5rem;
+  text-align: justify;
 `;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
 import { Header } from "@/components";
-import { Title, Subtitle, BackgroundImage } from "./index.styles";
+import {
+  TitleImage,
+  SubtitleImage,
+  BackgroundImage,
+  Row,
+  Column,
+  Title,
+  Content,
+} from "./index.styles";
 
 export default function Home() {
   return (
@@ -7,10 +15,64 @@ export default function Home() {
       <Header title="" />
       <BackgroundImage>
         <div>
-          <Title className="changa">GameChoose</Title>
-          <Subtitle className="changa">Você no controle.</Subtitle>
+          <TitleImage className="changa">GameChoose</TitleImage>
+          <SubtitleImage className="changa">Você no controle.</SubtitleImage>
         </div>
       </BackgroundImage>
+      <div>
+        <Row>
+          <Column>
+            <Title className="changa">O que somos?</Title>
+            <Content className="fira-branco">
+              GameChoose é mais que um site de compra e venda de jogos. Somos
+              uma comunidade gamer, que apoia todos os tipos de jogos, ação,
+              aventura, terror, até mesmo FIFA! Apoiamos desde desenvolvedoras
+              indie até grandes empresas. Aqui você poderá comprar com desconto
+              da concorrência, e ainda poderá ficar de olho nos{" "}
+              <span
+                style={{
+                  color: "#78BC61",
+                }}
+              >
+                gratuitos da semana
+              </span>
+              , onde você pode adquirir mensalmente quatro nomes aleatórios do
+              nosso catálogo!
+            </Content>
+          </Column>
+          <Column>
+            <Title className="changa">Sobre Nós</Title>
+            <Content className="fira-branco">
+              O GameChoose é um site totalmente fictício, que consiste em um
+              sistema de venda de jogos online. A ideia veio por um trabalho em
+              grupo, feito em 2019. Após alguns anos de sua conclusão, eu (
+              <span
+                style={{
+                  color: "#78BC61",
+                }}
+              >
+                Larissa Sousa
+              </span>
+              ) decidi recriar o projeto, porém do zero desta vez. Para sua
+              construção, utilizei Javascript no front-end (com React + Next) e
+              no back-end. Este projeto e outros meus podem ser acessados{" "}
+              <a
+                href="https://github.com/devlari"
+                style={{
+                  color: "#78BC61",
+                }}
+              >
+                aqui
+              </a>
+              .
+            </Content>
+          </Column>
+          <Column>
+            <Title className="changa">Contato</Title>
+            <Content className="fira-branco">Deixe uma mensagem!</Content>
+          </Column>
+        </Row>
+      </div>
     </>
   );
 }
